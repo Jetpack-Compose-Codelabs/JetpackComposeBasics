bff96e5e1f8f6de8a890f56e4bd858b0a6631e7e
arunkarka <arunkarka@gmail.com>
2022-10-23 15:01:47 -0500

    First Commit

---
+++ app/src/main/java/com/arun/jetpackcomposebasics/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.arun.jetpackcomposebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.arun.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBasicsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.primary) {
+                    Greeting("Greeting")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBasicsTheme {
+        Greeting("Android")
+    }
+}
+
